fb518c375875aa34d38202a1cf47bfea23e0ea6e
cristiantoong <cristiantoong@gmail.com>
2021-06-09 09:29:06 +0800

    installed gunicorn and whitenose

--- myrecipe/settings.py
+++ myrecipe/settings.py
@@ -65,12 +65,16 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'myrecipe.urls'
@@ -147,6 +151,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
 
 STATIC_URL = '/static/'
 
